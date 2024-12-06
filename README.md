ea148659787919373962d9b03bdf24d13ca443e7
toowiredd <lewistys420@gmail.com>
2024-12-07 01:45:40 +1100

    Initial commit: Basic repository structure and configuration files

---
+++ README.md
@@ -0,0 +1,1 @@
+# VPS Setup\n\nConfiguration and setup files for my VPS server.\n\n## Contents\n\n- Configuration files\n- Setup scripts\n- Documentation
---
+++ scripts/backup-script/backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+BACKUP_DIR="/tmp/backup_$TIMESTAMP"
+BUCKET_NAME="tysbucket"
+mkdir -p "$BACKUP_DIR"
+cp -r /etc/ssh "$BACKUP_DIR/"
+cp -r /etc/fail2ban "$BACKUP_DIR/"
+cp /etc/fstab "$BACKUP_DIR/"
+cp /etc/hostname "$BACKUP_DIR/"
+cp /etc/hosts "$BACKUP_DIR/"
+cd /tmp
+tar czf "backup_$TIMESTAMP.tar.gz" "backup_$TIMESTAMP"
+s3cmd put "backup_$TIMESTAMP.tar.gz" "s3://$BUCKET_NAME/system_backups/"
+rm -rf "$BACKUP_DIR"
+rm "backup_$TIMESTAMP.tar.gz"
